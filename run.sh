9e25b451e9c31cf59da8ce85dff3d2283c8d7675
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-02-15 23:48:40 +0200

    added option to customize server name via env

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "${SERVER_NAME}" ]]; then
+  SERVER_NAME='Teeworlds [dckr-'$(pwgen)']'
+fi
+
+echo 'sv_name ' $SERVER_NAME >> /${GAME_TYPE}.cfg
 echo "sv_rcon_password $(pwgen -s 12 1)" >> /${GAME_TYPE}.cfg
 cat /${GAME_TYPE}.cfg
 exec /usr/games/teeworlds-server -f /${GAME_TYPE}.cfg
