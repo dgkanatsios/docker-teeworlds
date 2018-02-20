c3ecafe040003afb55c29b2c541ea2f908ba40e7
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-02-20 12:26:54 +0200

    comments

--- stdoutprocessor.sh
+++ stdoutprocessor.sh
@@ -9,6 +9,7 @@ do
     #echo line so that docker can gather its logs from stdout
     echo $line
 
+    #message when a player is connected is like: [5a8bf70c][server]: player has entered the game. ClientID=1 addr=172.17.0.1:57587
     x=$(echo $line | grep 'has entered the game' | wc -l)
     toAdd=0
     if [ $x -eq 1 ]
@@ -16,6 +17,7 @@ do
        toAdd=1
     fi
     
+    #message when a player leaves is like [5a8bf70c][server]: '(1)nameless tee' has left the game
     y=$(echo $line | grep 'has left the game' | wc -l)
     if [ $y -eq 1 ]
     then
@@ -24,8 +26,9 @@ do
     
     if [ $x -eq 1 ] || [ $y -eq 1 ]
     then
-        #get value from the file
+        #get current connected count from the file
         connected=$(</tmp/connected)
+        #((..)) is the way for integer arithmetics on bash
         connected=$(($connected+$toAdd))
         echo $connected > /tmp/connected
     fi 
